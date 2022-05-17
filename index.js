fd55a6caa711d37f3b641c214fcbcc4e7dfaf636
Dinhomac <claudiojr113@gmail.com>
2022-05-17 20:41:38 +0000

    heroku installed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = require('./app');
 require('./db');
 
+// heroku server expects process to set the port like this, using a random port
 app.listen(process.env.PORT);
 console.log('Server on port 3001');
